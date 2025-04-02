59b2d5a42820902ac4a1a674f943f5bf87888144
AnshulGoyal589 <anshulgoyal589.com>
2025-04-02 22:24:36 +0530

    vw

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -5,18 +5,18 @@ export const routeConfig: RouteConfig = {
   roleRoutes: {
     admin: {
       allowedRoutes: ['/admin'],
-      redirectTo: '/',
+      redirectTo: '/admin/dashboard',
       defaultRoute: '/admin/dashboard'
     },
     manager: {
       allowedRoutes: ['/manager'],
-      redirectTo: '/',
+      redirectTo: '/manager/dashboard',
       defaultRoute: '/manager/dashboard'
     },
     customer: {
       // allowedRoutes: ['/home','/shops', '/query', '/order-history', '/profile','/track-order','/orders','/get-appointment','/listed-products','/product-details','/cart','/products','/my-orders','/add-new-product','/get-it-fixed','/ecommerce-orders', '/receipt','/missing-information','/missing-info'],
       allowedRoutes: ['/customer'],
-      redirectTo: '/',
+      redirectTo: '/customer/dashboard',
       defaultRoute: '/customer/dashboard'
     }
   } 
