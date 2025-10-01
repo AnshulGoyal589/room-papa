23d48a449dad779ec234482b98ed3e155bee3ac8
AnshulGoyal589 <anshulgoyal589@gmail.com>
2025-10-01 15:18:28 +0530

    costing issue resolved

--- src/components/customer/HomePage/UniqueProperties.tsx
+++ src/components/customer/HomePage/UniqueProperties.tsx
@@ -92,7 +92,7 @@ export default function UniqueProperties({ initialProperties }: UniqueProperties
                   <div className="text-right flex items-center justify-end gap-2 ">
                     <p className="text-base text-gray-500">Starting from</p>
                     <p className="text-xl font-bold text-gray-800">
-                      {property.costing?.currency} { property.costing.discountedPrice }
+                      {property.costing?.currency} { property?.costing?.discountedPrice }
                     </p>
                   </div>
                 </div>
